e161055783012cd554113c3227e7a809c9db7cb6
Juliya <52375078+YuliyaZabashta@users.noreply.github.com>
2022-12-05 21:28:59 +0500

    Update test1bit.php

--- test1bit.php
+++ test1bit.php
@@ -28,29 +28,40 @@ Template Post Type: post, page, product
 						<div class="tab-pane fade show active" id="pills-personal1" role="tabpanel" aria-labelledby="pills-home-tab">
 							<h4>Лиды</h4>
 							<div class="table-responsive">
-							<table class="table table-bordered">
-								<thead>
-									<tr class="grey">
-									<th scope="col">Название</th>
-									<th scope="col">Имя</th>
-									<th scope="col">Телефон</th>
-									<th scope="col">Дата создания</th>
-									<th scope="col">Время создания</th>
-									</tr>
-								</thead>
-								<?foreach($lides as $lide):?>
-								<tbody>
-								<tr>
-									<th scope="col"><?=$lide['TITLE']?></th>
-									<th scope="col"><?=$lide['NAME']?></th>
-									<th scope="col"><?= phone_format($lide['PHONE'][0]['VALUE']);?></th>
-									<th scope="col"><?=$new_date_format = date('m.d.Y', strtotime($lide['DATE_CREATE']));?></th>
-									<th scope="col"><?=$new_date_format = date('H:i', strtotime($lide['DATE_CREATE']));?></th>
-									</tr>
-								</tbody>
-								<?php endforeach?>
-							</table>
-						</div>
+								<table class="table table-bordered">
+									<thead>
+										<tr class="grey">
+										<th scope="col">Название</th>
+										<th scope="col">Имя</th>
+										<th scope="col">Телефон</th>
+										<th scope="col">Дата создания</th>
+										<th scope="col">Время создания</th>
+										</tr>
+									</thead>
+									<?foreach($lides as $lide):?>
+									<?php if($lide['CONTACT_ID'] == 0): ?>
+									<tbody>
+										<tr>
+											<th scope="col"><?=$lide['TITLE']?></th>
+											<th scope="col"><?=$lide['NAME']?></th>
+											<th scope="col"><?= phone_format($lide['PHONE'][0]['VALUE']);?></th>
+											<th scope="col"><?=$new_date_format = date('m.d.Y', strtotime($lide['DATE_CREATE']));?></th>
+											<th scope="col"><?=$new_date_format = date('H:i', strtotime($lide['DATE_CREATE']));?></th>
+									<?php endif;?>
+										<?foreach($contacts_lid as $contact_lid):?>
+										<?php if($lide['CONTACT_ID'] == $contact_lid['ID']):?>
+											<th scope="col"><?=$lide['TITLE']?></th>
+											<th scope="col"><?=$contact_lid['NAME']?></th>
+											<th scope="col"><?php if($lide['PHONE']):?><?= phone_format($lide['PHONE'][0]['VALUE']);?><?php elseif($contact_lid['PHONE']):?><?= phone_format($contact_lid['PHONE'][0]['VALUE']);?><?php else:?><?="Поле не заполнено"?><?php endif;?></th>
+											<th scope="col"><?=$new_date_format = date('m.d.Y', strtotime($lide['DATE_CREATE']));?></th>
+											<th scope="col"><?=$new_date_format = date('H:i', strtotime($lide['DATE_CREATE']));?></th>
+										<?php endif;?>
+										<?php endforeach?>		
+										</tr>
+									</tbody>
+									<?php endforeach?>
+								</table>
+							</div>
 						</div>
 						<div class="tab-pane fade" id="pills-abonement1" role="tabpanel" aria-labelledby="pills-profile-tab">
 							<a href="https://b24-mp0onw.bitrix24.site/crm_form_l4vvn/"  target="_blank" style="width: 30%; margin-bottom: 16px;" class="btn myform-btn btn-primary" type="submit">Дать обратную связь</a>
